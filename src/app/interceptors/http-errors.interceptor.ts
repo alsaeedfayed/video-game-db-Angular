d053f8d0295d9a26adfa7a2855e10e728fb59224
Alsaeed Fayed <alsaeed@rbt.sa>
2023-08-31 20:04:26 +0300

    home design

---
+++ src/app/interceptors/http-errors.interceptor.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core'
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http'
+import { Observable, catchError, throwError } from 'rxjs'
+
+@Injectable()
+export class HttpErrorsInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler,
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((err) => {
+        console.log(err)
+        return throwError(err)
+      }),
+    )
+  }
+}
