c54c96956e16220ffd9f3089838d62d74c94b225
Alsaeed Fayed <alsaeed@rbt.sa>
2023-09-01 12:12:55 +0300

    umdate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatSelectModule } from '@angular/material/select'
@@ -14,11 +14,18 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
 import { HomeComponent } from './components/home/home.component'
 import { HEADER_INTERCEPTOR } from './interceptors/http-headers.interceptor'
 import { ERROR_INTERCEPTOR } from './interceptors/http-errors.interceptor'
-import { NgxSpinnerModule } from 'ngx-spinner';
+import { NgxSpinnerModule } from 'ngx-spinner'
 import { DetailsComponent } from './components/details/details.component'
+import { NgxStarRatingModule } from 'ngx-star-rating'
+import { StarRatingModule } from 'angular-star-rating'
 
 @NgModule({
-  declarations: [AppComponent, SearchBarComponent, HomeComponent, DetailsComponent],
+  declarations: [
+    AppComponent,
+    SearchBarComponent,
+    HomeComponent,
+    DetailsComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -30,6 +37,9 @@ import { DetailsComponent } from './components/details/details.component'
     MatFormFieldModule,
     MatTabsModule,
     NgxSpinnerModule,
+    NgxStarRatingModule,
+    ReactiveFormsModule,
+    StarRatingModule.forRoot(),
   ],
   providers: [HEADER_INTERCEPTOR, ERROR_INTERCEPTOR],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
+  RatingForm!: FormGroup
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
